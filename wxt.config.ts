a1b175835c732b784580fa4e4157bbe40d340060
kkazzann <kamil.kazaniecki@beliani.net>
2026-07-07 14:06:09 +0200

    feat: add static manifest.key, new permissions and oauth2

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,8 +5,16 @@ export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['tabs', 'activeTab'],
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4kjsaFaJIAOasJYoxNqcfIdoO4w/0IjrECIoC9lc32SRoBeu7q/+iyViclAaeeeNSjrjd0h+/fGfcgWTgSpQuHhsub88MxzyzTz8mQpDS8DZcX4aps4NTOXBF0DWhgBh7iGJqDDT8lYQGyX321iON7ULKE5/2zwXt7LAHEIrhG+7t/RujufgCEIMXOk8bI6E798Ar2Da2vbG/Dq+OEvNDImScOQUr8LCBUrQkbKDf3zyqeljkaz/DYXrbiqW4CWwF36LAnyogArax00eXmFeNdStHhKSxDJFk+bzfYNlQ+K70/ALEwIpBx7OVGNieQDn2mhMPWM5W0pukx4aKoVqIwIDAQAB',
+    permissions: ['identity', 'tabs', 'activeTab', 'storage'],
     host_permissions: ['*://*.prologistics.info/*'],
+    oauth2: {
+      client_id: '620476405318-l5rhlmosiumfh3i514o0dg9ip8u30ole.apps.googleusercontent.com',
+      scopes: [
+        'https://www.googleapis.com/auth/calendar.readonly',
+        'https://www.googleapis.com/auth/userinfo.profile'
+      ],
+    },
   },
   hooks: {
     'build:manifestGenerated': (wxt, manifest) => {
