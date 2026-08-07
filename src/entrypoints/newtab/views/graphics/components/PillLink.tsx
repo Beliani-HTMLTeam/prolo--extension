daaa2e9663fb83f880300eda1bb588ce7f22d93d
Krzysztof <krzysieq95.mail@gmail.com>
2026-08-07 08:53:17 +0200

    fix: fix calendar image in top links

--- src/entrypoints/newtab/views/graphics/components/PillLink.tsx
+++ src/entrypoints/newtab/views/graphics/components/PillLink.tsx
@@ -11,10 +11,15 @@ interface PillLinkProps {
 }
 
 export default function PillLink({ item }: PillLinkProps) {
+  const currentDay = new Date().getDate();
+  let style = {};
+  if (item.image === 'calendar')
+    style = { objectFit: 'cover', objectPosition: `0px -${19 * (currentDay - 1)}px` };
+
   return (
     <a href={item.url} target="_blank" rel="noopener noreferrer" className={styles.pillLink}>
       {item.image ? (
-        <img src={`/icons/${item.image}.png`} alt={item.name} className={styles.pillImage} />
+        <img src={`/icons/${item.image}.png`} alt={item.name} className={styles.pillImage} style={style} />
       ) : item.icon ? (
         <Icon icon={item.icon} style={{ fontSize: '1.4rem' }} />
       ) : null}
