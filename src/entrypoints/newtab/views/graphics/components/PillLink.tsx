600915298129dbb5aa886cae8129fa6e4f88a550
Krzysztof <krzysieq95.mail@gmail.com>
2026-08-19 14:45:22 +0200

    fix: small fix for calendar pill image

--- src/entrypoints/newtab/views/graphics/components/PillLink.tsx
+++ src/entrypoints/newtab/views/graphics/components/PillLink.tsx
@@ -14,7 +14,7 @@ export default function PillLink({ item }: PillLinkProps) {
   const currentDay = new Date().getDate();
   let style = {};
   if (item.image === 'calendar')
-    style = { objectFit: 'cover', objectPosition: `0px -${19 * (currentDay - 1)}px` };
+    style = { objectFit: 'cover', objectPosition: `0px -${Math.floor(19.1667 * (currentDay - 1))}px` };
 
   return (
     <a href={item.url} target="_blank" rel="noopener noreferrer" className={styles.pillLink}>
